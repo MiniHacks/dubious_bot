ed74823b86c5c43184e4c1f8802e13dcd50352a0
iCalculated <fufa0001@gmail.com>
2022-09-17 15:32:27 +0200

    instrument scaling

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import time
 from optibook import ORDER_TYPE_LIMIT, SIDE_ASK, SIDE_BID
 from optibook.exchange_responses import InsertOrderResponse
 from optibook.synchronous_client import Exchange
+from collections import defaultdict
 import colors
 
 root = logging.getLogger()
@@ -14,8 +15,6 @@ logging.basicConfig(
     format=f"{colors.GREY}%(asctime)s.%(msecs)03d{colors.END} %(message)s",
     level=logging.DEBUG,
 )
-
-INSTRUMENT_ID = "SMALL_CHIPS_NEW_COUNTRY"
 
 
 def print_order_response(order_response: InsertOrderResponse):
@@ -50,13 +49,29 @@ def C(x):
 class Bot:
     def __init__(self, exchange):
         self.exchange = exchange
-        self.pnl = 0
+        self.pnl = self.exchange.get_pnl()
+        self.instruments = ["SMALL_CHIPS_NEW_COUNTRY", "SMALL_CHIPS"]
+        self.book = {}
+        self.is_trading = defaultdict(bool)
+
+        tradable_instruments = self.exchange.get_instruments()
+        for instrument_id in self.instruments:
+            if instrument_id not in tradable_instruments:
+                raise Exception(
+                    f"'{instrument_id}' does not exist. Options: {tradable_instruments}"
+                )
 
     def print_report(self):
         old_pnl, self.pnl = self.pnl, self.exchange.get_pnl()
         positions = self.exchange.get_positions()
-        my_trades = self.exchange.poll_new_trades(INSTRUMENT_ID)
-        all_market_trades = self.exchange.poll_new_trade_ticks(INSTRUMENT_ID)
+        my_trades, all_market_trades = [], []
+        for instrument_id in self.instruments:
+            my_trades += self.exchange.poll_new_trades(instrument_id)
+            all_market_trades += self.exchange.poll_new_trade_ticks(instrument_id)
+            logging.info(
+                f"{instrument_id}: bid={self.book[instrument_id].bids[0].price:.2f}, ask={self.book[instrument_id].asks[0].price:.2f}, spread={(self.book[instrument_id].asks[0].price - self.book[instrument_id].bids[0].price):.2f}"
+            )
+            logging.info(f"{self.book[instrument_id]}")
         logging.info(
             f"{colors.WHITE2}PNL: {old_pnl:.2f} -> {self.pnl:.2f}{colors.END}, (Δ: {c(self.pnl - old_pnl)}) \
             {colors.WHITE2}{len(my_trades)}/{len(all_market_trades)}{colors.END} trades"
@@ -65,63 +80,42 @@ class Bot:
             f"SMALL: {C(positions['SMALL_CHIPS'])} {C(positions['SMALL_CHIPS_NEW_COUNTRY'])}, δ: {C(positions['SMALL_CHIPS'] + positions['SMALL_CHIPS_NEW_COUNTRY'])}"  # \
             # TECH: {C(positions['TECH_INC'])} {C(positions['TECH_INC_NEW_COUNTRY'])}, δ: {C(positions['TECH_INC'] + positions['TECH_INC_NEW_COUNTRY'])}"
         )
-        logging.info(self.book[INSTRUMENT_ID])
-        logging.info(self.book[INSTRUMENT_ID])
-
-    def check_invariants(self):
-        tradable_instruments = self.exchange.get_instruments()
-
-        if INSTRUMENT_ID not in tradable_instruments:
-            return f"'{INSTRUMENT_ID}' does not exist. Options: {tradable_instruments}"
-
-        if tradable_instruments[INSTRUMENT_ID].paused:
-            return f"'{INSTRUMENT_ID}' is paused."
-
-        if (
-            not self.book[INSTRUMENT_ID]
-            or not self.book[INSTRUMENT_ID].bids
-            or not self.book[INSTRUMENT_ID].asks
-        ):
-            return f"'{INSTRUMENT_ID}' book error: {self.book[INSTRUMENT_ID]}'"
-
-        return ""
 
     def trade_cycle(self):
         if not self.exchange.is_connected():
             print(f"{colors.RED}Exchange not connected.{colors.END}")
             return
-        self.book[INSTRUMENT_ID] = self.exchange.get_last_price_book(INSTRUMENT_ID)
 
-        if error := self.check_invariants():
-            logging.error(error)
-            return
+        tradable_instruments = self.exchange.get_instruments()
+        for instrument_id in self.instruments:
+            self.book[instrument_id] = self.exchange.get_last_price_book(instrument_id)
+            self.is_trading[instrument_id] = tradable_instruments[instrument_id]
 
-        self.print_report()
+        for instrument_id in self.instruments:
+            self.exchange.delete_orders(instrument_id)
 
-        self.exchange.delete_orders(INSTRUMENT_ID)
-        logging.info(
-            f"{INSTRUMENT_ID}: bid={self.book[INSTRUMENT_ID].bids[0].price:.2f}, ask={self.book[INSTRUMENT_ID].asks[0].price:.2f}, spread={(self.book[INSTRUMENT_ID].asks[0].price - self.book[INSTRUMENT_ID].bids[0].price):.2f}"
-        )
         # try to improve the best bid and best ask by 10 cents
-        new_bid_price = self.book[INSTRUMENT_ID].bids[0].price + 0.1
-        new_ask_price = self.book[INSTRUMENT_ID].asks[0].price - 0.1
-        if new_ask_price - new_bid_price > 0.01:
-            bid_response: InsertOrderResponse = self.exchange.insert_order(
-                INSTRUMENT_ID,
-                price=new_bid_price,
-                volume=10,
-                side=SIDE_BID,
-                order_type=ORDER_TYPE_LIMIT,
-            )
-            print_order_response(bid_response)
-            ask_response: InsertOrderResponse = self.exchange.insert_order(
-                INSTRUMENT_ID,
-                price=new_ask_price,
-                volume=10,
-                side=SIDE_ASK,
-                order_type=ORDER_TYPE_LIMIT,
-            )
-            print_order_response(ask_response)
+        for instrument_id in self.instruments:
+            new_bid_price = self.book[instrument_id].bids[0].price + 0.1
+            new_ask_price = self.book[instrument_id].asks[0].price - 0.1
+            if new_ask_price - new_bid_price > 0.01:
+                bid_response: InsertOrderResponse = self.exchange.insert_order(
+                    instrument_id,
+                    price=new_bid_price,
+                    volume=10,
+                    side=SIDE_BID,
+                    order_type=ORDER_TYPE_LIMIT,
+                )
+                print_order_response(bid_response)
+                ask_response: InsertOrderResponse = self.exchange.insert_order(
+                    instrument_id,
+                    price=new_ask_price,
+                    volume=10,
+                    side=SIDE_ASK,
+                    order_type=ORDER_TYPE_LIMIT,
+                )
+                print_order_response(ask_response)
+        self.print_report()
 
 
 HOST = "hackzurich-1.optibook.net"
@@ -134,6 +128,7 @@ def main():
         host=HOST, username=USERNAME, password=PASSWORD, info_port=7001, exec_port=8001
     )
     exchange.connect()
+    time.sleep(3)
 
     bot = Bot(exchange)
 
